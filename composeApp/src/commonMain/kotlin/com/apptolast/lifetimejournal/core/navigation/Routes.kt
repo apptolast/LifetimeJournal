5b91e9cec931ece10169eb0511b8bd0a95e60770
Alberto <51128924+hgarciaalberto@users.noreply.github.com>
2025-04-15 20:24:27 +0100

    LJ-4 Project Setup: Koin, Kotzilla, Navigation and Dependencies (#4)
    - Integrated Koin for dependency injection.
    - Added Kotzilla SDK for analytics.
    - Configured Compose Navigation.
    - Updated dependencies (AndroidX Lifecycle, Material Icons Extended, Navigation Compose), Koin BOM, and Kotzilla SDK.
    - Enabled Kotlin compiler plugins (KSP, Kotzilla Plugin).
    - Set up BuildConfig for API keys.
    - Initialized Koin in Android and iOS apps.
    - Added Koin modules for app and view models.
    - Defined common navigation routes.
    - Added Kotzilla API key to local.properties and .gitignore.
    - Added Kotzilla repository to settings.gradle.kts.

---
+++ composeApp/src/commonMain/kotlin/com/apptolast/lifetimejournal/core/navigation/Routes.kt
@@ -0,0 +1,20 @@
+package com.apptolast.lifetimejournal.core.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object LoginDestination
+
+@Serializable
+object SettingDestination
+
+@Serializable
+object CreateBookDestination
+
+@Serializable
+object Home
+
+// @Serializable
+// data class DetailDestination {
+//    val movie: Movie
+// }
---
+++ composeApp/src/commonMain/kotlin/com/apptolast/lifetimejournal/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.apptolast.lifetimejournal.di
+
+import com.apptolast.kmptest.BuildConfig
+import io.kotzilla.sdk.analytics.koin.analytics
+import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.module
+
+val appModule = module {
+//    single(named("testApiKey")) { BuildConfig.TEST_API_KEY }
+//    factory { Greeting() }
+}
+
+val viewModelsModule = module {
+//    viewModelOf(::HomeListViewModel)
+}
+
+// expect val nativeModule: Module
+
+fun initKoin(appDeclaration: KoinAppDeclaration = {}) = startKoin {
+    appDeclaration()
+    modules(appModule, viewModelsModule)
+
+    // Kotzilla
+    analytics {
+        setApiKey(BuildConfig.KOTZILLA_API_KEY) // Available in the kotzilla.json file
+        setVersion("0.1.0")
+    }
+}
+
+// called by iOS
+fun initKoinIos() = initKoin {}
