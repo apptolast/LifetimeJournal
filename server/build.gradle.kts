0e81e761434734fcb3b11372400883de4225136d
Alberto <51128924+hgarciaalberto@users.noreply.github.com>
2025-05-24 19:31:01 +0100

    LJ-18 Configure Docker for server module (#13)
    - Added a Dockerfile to build and run the server module.
    - Updated `server/build.gradle.kts` to configure fat JAR creation with the name `lifetime-journal.jar` and Docker image settings.
    - Modified `settings.gradle.kts` to comment out `composeApp` and `shared` modules, focusing the build on the server module.
    - Changed the server port to `8080` and updated the root endpoint response in `Application.kt`.
    - Adjusted the server module version to `0.1.0`.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -5,15 +5,28 @@ plugins {
 }
 
 group = "com.apptolast.lifetimejournal"
-version = "1.0.0"
+version = "0.1.0"
 application {
     mainClass.set("com.apptolast.lifetimejournal.ApplicationKt")
     applicationDefaultJvmArgs =
         listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("lifetime-journal.jar")
+    }
+    docker {
+        jreVersion.set(JavaVersion.VERSION_17)
+
+        localImageName.set("lifetime-journal-docker-image")
+        imageTag.set("0.0.1-preview")
+
+    }
+}
+
 dependencies {
-    implementation(projects.shared)
+//    implementation(projects.shared)
     implementation(libs.logback)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
