9823815a896b6816d43cec2c203aca1dfc3ea30e
PabloHurtadoGonzalo <pablofullstackdevelopernrby@gmail.com>
2025-06-13 14:07:51 +0200

    Fix Dependencys for the server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -28,15 +28,24 @@ ktor {
 
 dependencies {
     //implementation(projects.shared)
-    implementation(libs.logback)
     // Cliente Redis - Lettuce (recomendado)
     implementation(libs.lettuce.core)
     // Inyección de dependencias
     implementation(libs.koin.ktor)
     // Serialización
-    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.simple.cache)
     implementation(libs.ktor.server.core)
+    implementation(libs.ktor.simple.redis.cache)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.h2)
+    implementation(libs.ktor.server.host.common)
+    implementation(libs.ktor.server.status.pages)
     implementation(libs.ktor.server.netty)
-    testImplementation(libs.ktor.server.tests)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
