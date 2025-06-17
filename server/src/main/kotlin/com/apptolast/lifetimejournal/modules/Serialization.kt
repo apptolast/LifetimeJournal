fc8f38231cc19cdad2b1f91f4c350bbe7346219b
hgarciaalberto <hgarcia.alberto@gmail.com>
2025-06-17 19:38:38 +0200

    Refactor: Improve JSON serialization configuration and add task routes
    - Updated `ContentNegotiation` plugin to use `kotlinx.json` with lenient, pretty print, and ignore unknown keys settings.
    - Added new routes for task management:
        - `GET /tasks`: Retrieve all tasks.
        - `POST /tasks`: Create a new task.
        - `GET /tasks/priority/{priority}`: Get tasks by priority.
        - `GET /tasks/{id}`: Get a task by ID.
        - `DELETE /tasks/{id}`: Delete a task by ID.
    - Implemented corresponding repository methods for task creation, retrieval, and deletion.
    - Handled `JsonConvertException` to return a `BadRequest` status.

--- server/src/main/kotlin/com/apptolast/lifetimejournal/modules/Serialization.kt
+++ server/src/main/kotlin/com/apptolast/lifetimejournal/modules/Serialization.kt
@@ -3,17 +3,31 @@ package com.apptolast.lifetimejournal.modules
 import com.apptolast.lifetimejournal.models.Priority
 import com.apptolast.lifetimejournal.models.Task
 import com.apptolast.lifetimejournal.models.repositories.TaskRepository
-import io.ktor.http.*
-import io.ktor.serialization.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.JsonConvertException
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization(repository: TaskRepository) {
+
     install(ContentNegotiation) {
-        json()
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            },
+        )
     }
     routing {
         route("/tasks") {
